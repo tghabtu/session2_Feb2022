ffc830936b4d1c849ad3ed2ff0bce86a7dc79b85
tghabtu <106480728+tghabtu@users.noreply.github.com>
2022-07-15 12:38:21 -0500

    commited

---
+++ src/main/java/variousConcepts/GitTest3.java
@@ -0,0 +1,27 @@
+package variousConcepts;
+
+public class GitTest3 {
+	
+
+	public static void main (String[]args) {
+		System.out.println("git test");
+		printMe();
+		printMe2();
+
+}
+	public  static void printMe() {
+		System.out.println("git test1");
+		
+		}
+	public static void printMe1() {
+		System.out.println("git test21");
+		
+		}
+	public static void printMe2() {
+		System.out.println("git test1");
+		
+			}
+	public static void printMe3() {
+		System.out.println("git test1");
+			}
+}
